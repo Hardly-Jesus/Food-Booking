792b52bca464d6a18d03f53daeb9dde3997aeaf4
kelvinkodad07 <kelvinkodaddiaz@gmail.com>
2026-03-14 20:21:06 -0300

    servicio y controllador de platoMenu completado

---
+++ ReservaBook/ReservaBook.Core.Aplication/Dtos/platoMenu/SavePlatoMenuRequesDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservaBook.Core.Aplication.Dtos.platoMenu
+{
+    public class SavePlatoMenuRequesDto
+    {
+     
+        [Required(ErrorMessage = "Debes indicar uno o varios plato")]
+        public List<int> IdPlatos { get; set; } = new List<int>();
+        [Range(1,int.MaxValue,ErrorMessage = "Debes indicar un menu valido")]
+        public int IdMenu { get; set; }
+    }
+}
